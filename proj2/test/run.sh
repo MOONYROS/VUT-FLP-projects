ac1865c63715ef4992dd0943fc340b5e8432681f
Ondrej Lukasek <ondrej@lukasek.cz>
2025-04-24 23:11:10 +0200

    feat: added some tests

---
+++ proj2/test/run.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+TMPFILE="./tmp_output"
+N_TESTS=8
+PASS=0
+
+for i in $(seq 1 $N_TESTS); do
+    echo "Running test $i..."
+    
+    ../flp24-log < "in$i.txt" > "$TMPFILE"
+    if diff -q "exp$i.txt" "$TMPFILE" > /dev/null; then
+        echo "Test $i passed"
+        PASS=$((PASS + 1))
+    else
+        echo "Test $i failed"
+        echo "Expected:"
+        cat "expected$i.txt"
+        echo "Got:"
+        cat "$TMPFILE"
+    fi
+    echo
+done
+
+rm -f "$TMPFILE"
+
+echo "PASSED: $PASS"
+echo "FAILED: $((N_TESTS - PASS))"
