5a579c15256c57dcf667910246595b4f31b43680
Ondrej Lukasek <ondrej@lukasek.cz>
2025-04-27 17:17:51 +0200

    feat: very thorough comments

--- proj2/Main.pl
+++ proj2/Main.pl
@@ -33,11 +33,24 @@ start :-
 
 % ========= ZPRACOVANI VSTUPU =========
 
-% pravidlo ma 4 parametry: aktualni stav, symbol pod hlavou, novy stav, akce
+%% rule(+CurrentState, +CurrentSymbol, +NewState, +Action)
+%
+% Reprezentuje pravidlo turingova stroje.
+%
+% @param CurrentState aktualni stav stroje
+% @param CurrentSymbol symbol pod hlavou
+% @param NewState novy stav po aplikaci pravidla
+% @param Action akce (symbol k zapisu, 'L' pro posun doleva nebo 'R' pro posun doprava)
+%
 :- dynamic rule/4.
 
-% vezmeme si vstup a rozdelime jej na
-% pravidla (vsechny radky krome posledniho) a pasku (posledni radek)
+%% process_file(+AllLines)
+%
+% Zpracuje vstupni radky souboru, rozdeli je na pravidla a pasku.
+% Nasledne odstrani duplicitni pravidla a spusti simulaci.
+%
+% @param AllLines seznam radku vstupniho souboru
+%
 process_file(AllLines) :-
     append(RuleLines, [Tape], AllLines),
     maplist(process_rule, RuleLines),
@@ -50,7 +63,13 @@ process_file(AllLines) :-
     
     process_tape(Tape).
 
-% zpracuje radek pravidla na ctverici rule (viz vyse)
+%% process_rule(+Rule)
+%
+% Prevede radek vstupu na pravidlo TS a vlozi jej do databaze.
+% Ocekava presny format pravidla, jinak nefunguje spravne.
+%
+% @param Rule seznam znaku tvoricich radek pravidla
+%
 process_rule(Rule) :-
     % vezmeme znaky na pozicich 0, 2, 4, 6 - ocekavame spravny format pravidla
     nth0(0, Rule, CurrentState),
@@ -61,7 +80,13 @@ process_rule(Rule) :-
     % pridame pravidlo do databaze
     assertz(rule(CurrentState, CurrentSymbol, NewState, Action)).
 
-% zpracuje pasku a spusti simulaci NTS
+%% process_tape(+Tape)
+%
+% Spusti simulaci turingova stroje s danou paskou a pocatecnim stavem 'S'.
+%
+% @param Tape pocatecni obsah pasky
+% @throws halt(1) pokud nelze dosahnout stavu 'F'
+%
 process_tape(Tape) :-
     ( 
         % nalevo je prazdno, napravo paska, pocatecni stav je S
@@ -73,7 +98,15 @@ process_tape(Tape) :-
 
 % ========= SIMULACE NTS =========
 
-% vytvoreni konfiguarace z pasky vlevo, vpravo a stavu
+%% create_configuration(+Left, +Right, +State, -Configuration)
+%
+% Vytvori aktualni konfiguraci stroje jako retezec.
+%
+% @param Left seznam symbolu nalevo od hlavy (v obracenem poradi)
+% @param Right seznam symbolu napravo od hlavy (prvni symbol je symbol po hlavou)
+% @param State aktualni stav stroje
+% @param Configuration konfigurace stroje v textovem formatu
+%
 create_configuration(Left, Right, State, Configuration) :-
     reverse(Left, RevLeft), % nejdriv si otocim pasku
     (
@@ -85,13 +118,36 @@ create_configuration(Left, Right, State, Configuration) :-
     atom_chars(Configuration, ConfigList).
 
 
-% simulace nedeterministickeho Turingova stroje
-% pokud jsme ve stavu F, vratime cestu az do konce
+%% simulate(+Left, +Right, +State, +History, +PrevConfigs, -Path)
+%
+% Simuluje nedeterministicky turinguv stroj.
+% Tato verze zpracovava koncovy stav.
+%
+% @param Left seznam symbolu nalevo od hlavy (v obracenem poradi)
+% @param Right seznam symbolu napravo od hlavy (prvni symbol je symbol po hlavou)
+% @param State aktualni stav stroje ('F')
+% @param History seznam navstivenych konfiguraci (nevyuzito)
+% @param PrevConfigs seznam predchozich konfiguraci
+% @param Path vysledna cesta od pocatecni k cilove konfiguraci
+%
 simulate(Left, Right, 'F', _, PrevConfigs, Path) :-
     create_configuration(Left, Right, 'F', FinalConfig),
     reverse([FinalConfig|PrevConfigs], Path),
     !.
-% pokud nejsme v koncovem stavu, pokracujeme
+
+%% simulate(+Left, +Right, +State, +History, +PrevConfigs, -Path)
+%
+% Simuluje nedeterministicky turinguv stroj.
+% Tato verze zpracovava nekoncove stavy.
+%
+% @param Left seznam symbolu nalevo od hlavy (v obracenem poradi)
+% @param Right seznam symbolu napravo od hlavy (prvni symbol je symbol po hlavou)
+% @param State aktualni stav stroje (ne 'F')
+% @param History seznam navstivenych konfiguraci (detekce cyklu)
+% @param PrevConfigs seznam predchozich konfiguraci
+% @param Path vysledna cesta od pocatecni k cilove konfiguraci
+% @throws fail pokud nelze najit cestu k cilove konfiguraci
+%
 simulate(Left, Right, State, History, PrevConfigs, Path) :-
     % kontrola cykleni
     \+ member((Left, Right, State), History),
@@ -121,16 +177,39 @@ simulate(Left, Right, State, History, PrevConfigs, Path) :-
             apply_rule(Left, Right, Rule, NewHistory, NewPrevConfigs, Path)
     ).
 
-% ziskani pravidla vedouciho do F
+%% get_finishing_rule(+Rules, -Rule)
+%
+% Najde pravidlo, vedouci do koncoveho stavu 'F'.
+%
+% @param Rules seznam pravidel k prohledani
+% @param Rule nalezene pravidlo vedouci do 'F'
+%
 get_finishing_rule(Rules, Rule) :-
     member(Rule, Rules),
     Rule = rule(_, _, 'F', _).
 
-% ziskani aktualniho symbolu pod hlavou
+%% get_current_symbol(+Right, -Symbol)
+%
+% Ziska aktualni symbol pod hlavou nebo mezeru, pokud jsme na (pravem) konci pasky.
+%
+% @param Right seznam symbolu napravo od hlavy (prvni symbol je symbol po hlavou)
+% @param Symbol aktualni symbol pod hlavou nebo mezernik (v pripade prazdne pasky)
+%
 get_current_symbol([], ' ').
 get_current_symbol([Head|_], Head).
 
-% aplikace vybraneho pravidla
+%% apply_rule(+Left, +Right, +Rule, +History, +PrevConfigs, -Path)
+%
+% Aplikuje dane pravidlo stroje podle typu akce.
+%
+% @param Left seznam symbolu nalevo od hlavy (v obracenem poradi)
+% @param Right seznam symbolu napravo od hlavy (prvni symbol je symbol po hlavou)
+% @param Rule pravidlo k aplikaci ve formatu rule(_, _, NewState, Action)
+% @param History seznam navstivenych konfiguraci
+% @param PrevConfigs seznam predchozich konfiguraci
+% @param Path vysledna cesta od pocatecni k cilove konfiguraci
+% @throws fail pokud nelze aplikovat pravidlo (posun pred levy okraj pasky)
+%
 apply_rule(Left, Right, rule(_, _, NewState, Action), History, PrevConfigs, Path) :-
     (
         Action = 'L' -> % posun doleva
@@ -144,16 +223,36 @@ apply_rule(Left, Right, rule(_, _, NewState, Action), History, PrevConfigs, Path
         Action = 'R' -> % posun doprava nebo zapis symbolu
             apply_right_move(Left, Right, NewState, History, PrevConfigs, Path)
         ;
-            apply_symbol_write(Left, Right, Action, NewState, History, PrevConfigs, Path)
+            apply_write(Left, Right, Action, NewState, History, PrevConfigs, Path)
     ).
 
-% aplikace pohybu doleva
+%% apply_left_move(+Left, +Right, +NewState, +History, +PrevConfigs, -Path)
+%
+% Provede posun hlavy doleva a pokracuje v simulaci.
+%
+% @param Left seznam symbolu nalevo od hlavy (v obracenem poradi)
+% @param Right seznam symbolu napravo od hlavy (prvni symbol je symbol po hlavou)
+% @param NewState novy stav stroje po aplikaci pravidla
+% @param History seznam navstivenych konfiguraci
+% @param PrevConfigs seznam predchozich konfiguraci
+% @param Path vysledna cesta od pocatecni k cilove konfiguraci
+%
 apply_left_move(Left, Right, NewState, History, PrevConfigs, Path) :-
     Left = [NewHead|NewLeft],
     Right = [Head|Tail],
     simulate(NewLeft, [NewHead, Head|Tail], NewState, History, PrevConfigs, Path).
 
-% aplikace pohybu doprava
+%% apply_right_move(+Left, +Right, +NewState, +History, +PrevConfigs, -Path)
+%
+% Provede posun hlavy doprava a pokracuje v simulaci.
+%
+% @param Left seznam symbolu nalevo od hlavy (v obracenem poradi)
+% @param Right seznam symbolu napravo od hlavy (prvni symbol je symbol po hlavou)
+% @param NewState novy stav stroje po aplikaci pravidla
+% @param History seznam navstivenych konfiguraci
+% @param PrevConfigs seznam predchozich konfiguraci
+% @param Path vysledna cesta od pocatecni k cilove konfiguraci
+%
 apply_right_move(Left, Right, NewState, History, PrevConfigs, Path) :-
     Right = [Head|Tail],
     (
@@ -163,13 +262,31 @@ apply_right_move(Left, Right, NewState, History, PrevConfigs, Path) :-
             simulate([Head|Left], [' '], NewState, History, PrevConfigs, Path)
     ).
 
-% aplikace zapisu symbolu
-apply_symbol_write(Left, Right, Symbol, NewState, History, PrevConfigs, Path) :-
+%% apply_write(+Left, +Right, +Symbol, +NewState, +History, +PrevConfigs, -Path)
+%
+% prepise aktualni symbol pod hlavou a pokracuje v simulaci.
+%
+% @param Left seznam symbolu nalevo od hlavy (v obracenem poradi)
+% @param Right seznam symbolu napravo od hlavy (prvni symbol je symbol po hlavou)
+% @param Symbol novy symbol k zapisu pod hlavou
+% @param NewState novy stav stroje po aplikaci pravidla
+% @param History seznam navstivenych konfiguraci
+% @param PrevConfigs seznam predchozich konfiguraci
+% @param Path vysledna cesta od pocatecni k cilove konfiguraci
+%
+apply_write(Left, Right, Symbol, NewState, History, PrevConfigs, Path) :-
     Right = [_|Tail],
     simulate(Left, [Symbol|Tail], NewState, History, PrevConfigs, Path).
 
 % ========= VYPIS CESTY =========
 
+%% print_path(+Path)
+%
+% Vypise sekvenci konfiguraci na standardni vystup.
+% Kazdou na samostatny radek.
+%
+% @param Path seznam konfiguraci k vypsani
+%
 print_path([]) :- !.
 print_path([Config|Rest]) :-
     writeln(Config),
